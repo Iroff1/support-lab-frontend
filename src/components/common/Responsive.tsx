624b95e8fb6ae197debf3ca125950cdaacf842be
sungohki <sokim1201@naver.com>
2025-04-23 14:39:08 +0900

    feat: 반응형 프레임 컴포넌트 추가

---
+++ src/components/common/Responsive.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ResponsiveBlock = styled.div`
+  padding: 0 10px;
+  width: 1440px;
+  margin: 0 auto;
+  background-color: white;
+
+  transition: 0.2s ease width;
+
+  @media (max-width: 1440px) {
+    width: 100%;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    max-width: 745px;
+  }
+  @media (max-width: 425px) {
+    width: 100%;
+    max-width: 393px;
+  }
+`;
+
+const Responsive: React.FC<any> = ({ children, ...restProps }) => {
+  return <ResponsiveBlock {...restProps}>{children}</ResponsiveBlock>;
+};
+
+export default Responsive;
