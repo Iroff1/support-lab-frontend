2c72cbd1058b8f44e0058a7c2642fb957c4b6df6
sungohki <sokim1201@naver.com>
2025-04-28 16:46:02 +0900

    feat: 메인 커리큘럼 박스 컴포넌트 추가

---
+++ src/components/main/MainCurriculamBox.tsx
@@ -0,0 +1,121 @@
+import styled, { css } from 'styled-components';
+import MainMessageBox from './MainMessageBox';
+import tranlateFontSize from '@hooks/tranlateFontSize';
+import palette from '@colors/index';
+
+const MainCurriculamBoxBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const CurriculamList = styled.div`
+  width: 952px;
+  height: 195px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CurriculamItem = styled.div`
+  width: 220px;
+  height: 195px;
+  padding: 20px;
+  padding-right: 0px;
+  border-radius: 10px;
+  background-color: #ffffff20;
+  box-shadow: 0px 4px 10px 0px #00000040;
+  color: ${palette.black.B700};
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 20px;
+
+  img {
+    width: 48px;
+    heigth: 48px;
+  }
+
+  div {
+    & > p:nth-child(1) {
+      ${css(tranlateFontSize('B_21'))}
+    }
+    & > p:nth-child(2) {
+      margin-top: 10px;
+      ${css(tranlateFontSize('R_17'))}
+    }
+  }
+`;
+
+const RightArrow = () => {
+  return (
+    <img
+      style={{ width: '24px', height: '24px' }}
+      src={require('@images/icon_rightArrow.png')}
+      alt="rightArrow"
+    />
+  );
+};
+
+const MainCurriculamBox = () => {
+  return (
+    <MainCurriculamBoxBlock>
+      <MainMessageBox>
+        <h2>이렇게 작성합니다.</h2>
+      </MainMessageBox>
+
+      <CurriculamList>
+        <CurriculamItem>
+          <img src={require('@images/curriculam_1.png')} alt="icon1" />
+          <div>
+            <p>무료 전화 상담</p>
+            <p>
+              궁금한 것은 무엇이든
+              <br />
+              질문해주세요.
+            </p>
+          </div>
+        </CurriculamItem>
+        <RightArrow />
+        <CurriculamItem>
+          <img src={require('@images/curriculam_2.png')} alt="icon2" />
+          <div>
+            <p>솔루션 결제</p>
+            <p>
+              상담 일정을 정하고 원하는
+              <br />
+              솔루션을 결제해주세요.
+            </p>
+          </div>
+        </CurriculamItem>
+        <RightArrow />
+        <CurriculamItem>
+          <img src={require('@images/curriculam_3.png')} alt="icon3" />
+          <div>
+            <p>멘토링</p>
+            <p>
+              전문가 멘토링을 통해서
+              <br />
+              아이템 정보를 전달해주세요.
+            </p>
+          </div>
+        </CurriculamItem>
+        <RightArrow />
+        <CurriculamItem>
+          <img src={require('@images/curriculam_4.png')} alt="icon4" />
+          <div>
+            <p>사업계획서 초안 받기</p>
+            <p>
+              아이디어 전달 후 10분 내<br />
+              결과물을 드립니다.
+            </p>
+          </div>
+        </CurriculamItem>
+      </CurriculamList>
+    </MainCurriculamBoxBlock>
+  );
+};
+
+export default MainCurriculamBox;
