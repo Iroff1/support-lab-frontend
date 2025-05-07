79443c5f29767c2e694820bf39215f999588bc17
sungohki <sokim1201@naver.com>
2025-05-07 12:59:17 +0900

    chore: 라우팅 페이지 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AuthLayout from '@layout/AuthLayout';
 import MainLayout from '@layout/MainLayout';
 import AuthFindEmailPage from '@pages/auth/AuthFindEmailPage';
+import AuthFindPasswordPage from '@pages/auth/AuthFindPasswordPage';
 import AuthLoginPage from '@pages/auth/AuthLoginPage';
 import AuthRegisterPage from '@pages/auth/AuthRegisterPage';
 import MainPage from '@pages/MainPage';
@@ -15,9 +16,10 @@ const App = () => {
       <Route path="/auth" element={<AuthLayout />}>
         <Route index element={<AuthLoginPage />} />
         <Route path="register" element={<AuthRegisterPage />} />
-        <Route path="find" element={<AuthFindEmailPage />} />
-        <Route path="find/:id" element={<AuthFindEmailPage />} />
+        <Route path="find/email" element={<AuthFindEmailPage />} />
+        <Route path="find/password" element={<AuthFindPasswordPage />} />
       </Route>
+      <Route path="*" element={<>Not Found</>} />
     </Routes>
   );
 };
