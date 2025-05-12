21b267b2b19ec2fc43c1337363f653d3e19f91ac
sungohki <sokim1201@naver.com>
2025-05-12 16:19:31 +0900

    fix: 문구 수정, 배경 이미지 추가

--- src/components/main/MainLastCoverBox.tsx
+++ src/components/main/MainLastCoverBox.tsx
@@ -1,71 +1,68 @@
 import styled, { css } from 'styled-components';
-import { ConsultButton } from './MainCoverBox';
 import translateFontSize from '@utils/translateFontSize';
+import palette from '@assets/colors';
+import ConsultButton from '@components/common/ConsultButton';
+
+const LAST_COVER = require('@assets/images/main/last_cover.png');
 
 const MainLastCoverBoxBlock = styled.div`
   width: 100%;
   height: calc(100vh - 64px);
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 20px;
-  background-color: lightgray;
+  background: url(${LAST_COVER}) center/cover no-repeat;
 `;
 
-const CoverContentBox = styled.div`
-  width: 702px;
+const CoverBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #33333320;
+`;
+
+const CoverBoxContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 20px;
-  text-align: center;
-  background-color: #33333320;
-  border-radius: 15px;
-  padding: 25px 0;
+  color: ${palette.black.B10};
 
-  & > div {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-
-    h2 {
-      ${css(translateFontSize('B_48'))}
-    }
-    p {
-      ${css(translateFontSize('R_22'))}
-    }
+  h2 {
+    ${css(translateFontSize('B_48'))}
+    text-wrap: wrap;
+  }
+  p {
+    ${css(translateFontSize('R_22'))}
   }
 `;
 
 const MainLastCoverBox = () => {
   return (
     <MainLastCoverBoxBlock>
-      <CoverContentBox>
-        <div>
+      <CoverBox>
+        <CoverBoxContent>
           <h2>
-            어떤 걸 해야할지 모르겠나요?
+            그.래.서!
             <br />
-            사용 방법이 어려우신가요?
+            무엇부터 시작해야 할지 막막하신가요?
           </h2>
+
           <p>
-            걱정하지 마세요.
+            지금 바로 전화주세요.
             <br />
             <br />
-            사업계획서 초안 솔루션을
+            지원사업 전문가가 당신의 상황에 꼭 맞는 해답을 제시해 드리고
             <br />
-            사용하고 싶은 분은 바로 전화 주세요.
-            <br />
-            <br />
-            지원사업 전문가가 무료 상담을 해드리고
-            <br />
-            솔루션 사용 방법을 상세하게 안내해드립니다.
+            솔루션 사용법도 처음부터 끝까지, 친절하게 알려드립니다.
           </p>
-        </div>
-        <ConsultButton>무료 상담</ConsultButton>
-      </CoverContentBox>
+        </CoverBoxContent>
+
+        <ConsultButton location="body">무료 상담</ConsultButton>
+      </CoverBox>
     </MainLastCoverBoxBlock>
   );
 };
