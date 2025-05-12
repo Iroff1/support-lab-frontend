ad412bbd60965b640197778b7001e5b4f8e8cac8
sungohki <sokim1201@naver.com>
2025-05-12 13:17:21 +0900

    fix: 헤더 추가, 헤더 고려한 상단 간격 조정

--- src/layout/CSLayout.tsx
+++ src/layout/CSLayout.tsx
@@ -1,18 +1,22 @@
+import Header from '@components/common/Header';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CSLayoutBlock = styled.div`
   display: flex;
-  padding-top: 80px;
+  padding-top: 104px;
   flex-direction: column;
   align-items: center;
 `;
 
 const CsLayout = () => {
   return (
-    <CSLayoutBlock>
-      <Outlet />
-    </CSLayoutBlock>
+    <>
+      <Header />
+      <CSLayoutBlock>
+        <Outlet />
+      </CSLayoutBlock>
+    </>
   );
 };
 export default CsLayout;
