4487a5e67ffc3c2b4d2ebe81496a09bd5d8782a8
sungohki <sokim1201@naver.com>
2025-05-13 15:14:03 +0900

    feat: Provider 적용, 전역 상태 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
+import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import './index.css';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
