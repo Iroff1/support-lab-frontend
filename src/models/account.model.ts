4102bc6b79795cb2f4373fd6c2897566cd293eb9
sungohki <sokim1201@naver.com>
2025-05-14 18:49:50 +0900

    feat: IAuthError 인터페이스 추가

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,19 +1,18 @@
-export interface IAccount {
-  login: ILogin;
-  register: IRegister;
+export interface IAuthError {
+  [key: string]: string;
 }
 
 export interface ILogin {
   email: string;
   password: string;
+  error: IAuthError;
 }
 
-export interface IRegister {
-  email: string;
+export interface IRegister extends ILogin {
   username: string;
-  password: string;
   passwordConfirm: string;
   contact: string;
   personalInfoAgreement: boolean;
   marketingAgreement: boolean;
+  error: IAuthError;
 }
