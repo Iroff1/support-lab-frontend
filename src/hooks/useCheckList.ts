f8f0f1494567b5e51927d25864033f44a231c330
sungohki <sokim1201@naver.com>
2025-05-28 14:53:50 +0900

    feat: 유효성 체크리스트 훅 추가

---
+++ src/hooks/useCheckList.ts
@@ -0,0 +1,24 @@
+import { IAuthChecker } from '@models/auth.model';
+import { useEffect, useState } from 'react';
+
+const useCheckList = <T>(init: IAuthChecker<T>) => {
+  const [checkList, setCheckList] = useState<IAuthChecker<T>>(init);
+  const [checkResult, setCheckResult] = useState(false);
+
+  const handleCheckList = (key: keyof T, result: boolean) => {
+    if (checkList[key] === result) return;
+    setCheckList((prev) => ({ ...prev, [key]: result }));
+  };
+
+  const confirmCheckList = () => {
+    for (const key of Object.keys(checkList))
+      if (!checkList[key as keyof T]) return false;
+    return true;
+  };
+
+  useEffect(() => {
+    setCheckResult(confirmCheckList());
+  }, [checkList]);
+  return { checkList, handleCheckList, checkResult };
+};
+export default useCheckList;
