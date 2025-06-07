4175b44a3843d85a06608b45aace4b2cd1e9a647
sungohki <sokim1201@naver.com>
2025-06-08 03:06:32 +0900

    feat: dotenv-webpack 설치 및 적용용

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -61,6 +63,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+    new Dotenv(),
   ],
   devtool: 'inline-source-map',
   devServer: {
