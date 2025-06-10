1986e53c3d30080b83360cffef83b915a1886537
sungohki <sokim1201@naver.com>
2025-06-10 13:13:20 +0900

    feat: express 모듈 설치 (빌드 파일 제공 서버용도)

---
+++ web.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 8001;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (_, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.get('/{*any}', function (_, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on PORT ${PORT}`);
+});
