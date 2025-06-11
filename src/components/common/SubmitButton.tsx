9cf26c6bf4912682ee8a45b7e33ccbb292a839fe
sungohki <sokim1201@naver.com>
2025-06-11 18:09:38 +0900

    feat: Submit 버튼 스타일 종류 2가지 -> 3가지로 변경

--- src/components/common/SubmitButton.tsx
+++ src/components/common/SubmitButton.tsx
@@ -3,15 +3,23 @@ import translateFontSize from '@utils/translateFontSize';
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-const SubmitButtonBlock = styled.button<{ $inverse?: boolean }>`
+export const SubmitButtonBlock = styled.button<{
+  $maxWidth?: string;
+  $style?: 'inverse' | 'default' | 'negative';
+}>`
   width: 100%;
+  ${({ $maxWidth }) =>
+    $maxWidth &&
+    css`
+      max-width: ${$maxWidth};
+    `}
   height: 48px;
   border-radius: 8px;
   padding: 12px 10px;
   border: 0;
   ${css(translateFontSize('SB_18'))};
   color: ${palette.black.white};
-  transition: 0.2s ease background-color;
+  transition: 0.2s ease background-color, 0.2s ease box-shadow;
   background-color: ${palette.main.main};
   cursor: pointer;
 
@@ -20,19 +28,29 @@ const SubmitButtonBlock = styled.button<{ $inverse?: boolean }>`
     cursor: not-allowed;
   }
 
-  ${({ $inverse: inverse }) =>
-    inverse &&
-    css`
-      background-color: transparent;
-      color: ${palette.main.main};
-      border: 1px solid ${palette.main.main};
-    `}
+  ${({ $style }) =>
+    $style === 'inverse'
+      ? css`
+          background-color: transparent;
+          color: ${palette.main.main};
+          border: 1px solid ${palette.main.main};
+        `
+      : $style === 'negative'
+      ? css`
+          background-color: ${palette.black.B50};
+        `
+      : null}
+
+  &:hover {
+    box-shadow: 0 0 8px 0 #00000040;
+  }
 `;
 
 interface ISubmitButton {
   children: string;
   disabled?: boolean;
-  $inverse?: boolean;
+  $maxWidth?: string;
+  $style?: 'inverse' | 'default' | 'negative';
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   ref?: React.RefObject<HTMLButtonElement | null>;
 }
@@ -41,20 +59,23 @@ const SubmitButton: React.FC<ISubmitButton> = ({
   children,
   ref,
   disabled,
-  $inverse = false,
+  $maxWidth,
+  $style = 'default',
   onClick,
 }) => {
   return (
-    <>
-      <SubmitButtonBlock
-        ref={ref}
-        disabled={disabled}
-        onClick={onClick}
-        $inverse={$inverse}
-      >
-        {children}
-      </SubmitButtonBlock>
-    </>
+    <SubmitButtonBlock
+      ref={ref}
+      disabled={disabled}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick && onClick(e);
+      }}
+      $style={$style}
+      $maxWidth={$maxWidth}
+    >
+      {children}
+    </SubmitButtonBlock>
   );
 };
 
