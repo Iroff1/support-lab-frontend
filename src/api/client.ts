2299bfc864d68fddfe1ce5fbf3273a96c22fd4fe
sungohki <sokim1201@naver.com>
2025-06-11 11:09:57 +0900

    chore: dev server 포트 번호 변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_SERVER;
+const ServerURL = `https://${process.env.REACT_APP_API_SERVER}/api`;
 
 const client = axios.create({
   // baseURL: '/api',
-  baseURL: `https://${baseURL}/api`,
+  baseURL: ServerURL,
   headers: {
     'Content-Type': 'application/json',
 
     // 추가
-    'Access-Control-Allow-Origin': `http://localhost:3000`,
+    'Access-Control-Allow-Origin': `http://localhost:${process.env.REACT_APP_PORT}`,
     'Access-Control-Allow-Credentials': 'true',
   },
 });
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,19 +5,19 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    port: 3000,
+    port: process.env.REACT_APP_PORT,
     host: 'localhost',
     static: './dist',
     hot: true,
     open: true,
     historyApiFallback: true,
-    // proxy: [
-    //   {
-    //     context: ['/api'],
-    //     target: `https://${process.env.REACT_APP_API_SERVER}`,
-    //     changeOrigin: true,
-    //     withCredentials: true,
-    //   },
-    // ],
+    proxy: [
+      {
+        context: ['/api'],
+        target: `https://${process.env.REACT_APP_API_SERVER}`,
+        changeOrigin: true,
+        withCredentials: true,
+      },
+    ],
   },
 });
