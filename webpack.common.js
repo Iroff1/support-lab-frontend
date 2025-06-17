84c34de87d964ba8ad76cc0a0d4734d9dcd86bc6
sungohki <sokim1201@naver.com>
2025-06-17 15:27:29 +0900

    feat: webpack favicon 설정

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|mp4|webp)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|mp4|webp|ico|json)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'static/[name][ext]',
@@ -60,6 +60,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      favicon: './public/favicon.png',
     }),
     new Dotenv(),
   ],
