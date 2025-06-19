809c7f798af1f81cc250b652a2c49fc568a27f2f
sungohki <sokim1201@naver.com>
2025-06-19 11:16:34 +0900

    chore: express서버 파일명 정정, 오탈자 정정

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const path = require('path');
 const express = require('express');
-const app = express();
-const PORT = 8001;
+const dotenv = require('dotenv');
 
+const app = express();
+dotenv.config();
+
+/** Allow to resonce to provide static html */
 app.use(express.static(path.join(__dirname, 'dist')));
 
+/** Path Routing
+ * @description Allow whole routing to React static app
+ */
 app.get('/', function (_, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
-
 app.get('*', function (_, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(PORT, () => {
+/** Server Start */
+app.listen(process.env.REACT_APP_PORT || 8001, () => {
   console.log(`server started on PORT ${PORT}`);
 });
